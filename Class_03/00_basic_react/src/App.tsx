30946e2456140aed64414495ac2691ac2f79c0ca
M Nasir Khan <immnk97@gmail.com>
2024-10-01 23:15:27 +0500

    class 03 code

---
+++ Class_03/00_basic_react/src/App.tsx
@@ -0,0 +1,15 @@
+import Name from "./Name"
+import Student from "./Student"
+
+function App() {
+
+  return (
+    <>
+     <h1>Basic React App</h1>
+     <Name />
+     <Student />
+    </>
+  )
+}
+
+export default App
