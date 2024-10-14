196c4c2218b6094c3e18770c58dc5acd25dd614a
M Nasir Khan <immnk97@gmail.com>
2024-10-15 00:28:50 +0500

    Class 06 code

---
+++ Class_06/03_nextjs/src/app/news/layout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const pageLayout = ({ children }) => {
+  //   return <div>Hello from news Layout {children}</div>;
+  return <div> {children}</div>;
+};
+
+export default pageLayout;
+
+// import React, { ReactNode } from "react";
+
+// interface PageLayoutProps {
+//   children: ReactNode; // Define the type for children
+// }
+
+// const PageLayout: React.FC<PageLayoutProps> = ({ children }) => {
+//   return <div>{children}</div>;
+// };
+
+// export default PageLayout;
