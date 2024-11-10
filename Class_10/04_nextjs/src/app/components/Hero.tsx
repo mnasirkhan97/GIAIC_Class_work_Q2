fa9cb5af32957933a57206a22eb79a6fd77004c1
M Nasir Khan <immnk97@gmail.com>
2024-11-11 00:01:01 +0500

    class 10 code

---
+++ Class_10/04_nextjs/src/app/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <main className="flex gap-5 mx-4 my-2">
+      <div className="bg-yellow-800 w-6/12 h-40"></div>
+      <div className="bg-slate-800 w-6/12 h-40"></div>
+    </main>
+  );
+};
+
+export default Hero;
