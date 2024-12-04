33a50af2a11e75752884cd41dd414b626cb90f4b
M Nasir Khan <immnk97@gmail.com>
2024-12-05 00:03:25 +0500

    Add styling

--- Class_13/07_nextjs/src/components/Navbar.tsx
+++ Class_13/07_nextjs/src/components/Navbar.tsx
@@ -4,9 +4,9 @@ function Navbar() {
   return (
     <div className="bg-purple-400 h-[40px] flex justify-around items-center">
       <div>logo</div>
-      <div className="flex">
+      <div className="flex space-x-10">
         <div>
-          <ul className="flex">
+          <ul className="flex space-x-3">
             <li>About</li>
             <li>Services</li>
             <li>Contact</li>
