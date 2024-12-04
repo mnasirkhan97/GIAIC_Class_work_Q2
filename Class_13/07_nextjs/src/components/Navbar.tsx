27eecf56dbba21caa5831098ecd05122e587c3ae
M Nasir Khan <immnk97@gmail.com>
2024-12-04 23:29:48 +0500

    create navbar

--- Class_13/07_nextjs/src/components/Navbar.tsx
+++ Class_13/07_nextjs/src/components/Navbar.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Navbar() {
-  return <div></div>;
+  return (
+    <div>
+      <div>logo</div>
+      <div>
+        <div>
+          <ul>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div>
+          <button>Login</button>
+          <button>Register</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
